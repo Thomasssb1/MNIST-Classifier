86b5b32b4a51204d94d77c276db0b43aa504f718
Thomas Beer <thomas.beer04@outlook.com>
2024-12-22 17:45:56 +0000

    LDA implementation

--- system.py
+++ system.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 
-def addLabels(images, labels):
+def addLabels(images, labels, datatype=None):
     """"""
     new_size = (images.shape[0], images.shape[1] + 1)
-    temp = np.zeros(new_size)
+    temp = np.zeros(new_size, dtype=datatype)
     temp[:, 1:] = images
     temp[:, 0] = labels
     return temp
@@ -60,20 +60,75 @@ def findBestFeatures(labelled_pca_images, N=9):
     return features
 
 
+def computeMeanVectors(labelled_images):
+    temp = np.zeros((10, labelled_images.shape[1] - 1))
+
+    for label in range(0, 10):
+        mean = np.mean(labelled_images[labelled_images[:, 0] == label, 1:], axis=0)
+        temp[label, :] = mean
+    return temp
+
+
+def computeWithinClassScatterMatrices(labelled_images, mean_vectors):
+    feature_count = labelled_images.shape[1] - 1
+    temp = np.zeros((feature_count, feature_count))
+
+    for label, mean in zip(range(0, 10), mean_vectors):
+        class_scatter = np.zeros(temp.shape)
+        for row in labelled_images[labelled_images[:, 0] == label, 1:]:
+            row, mean = row.reshape(feature_count, 1), mean.reshape(feature_count, 1)
+            class_scatter += (row - mean).dot((row - mean).T)
+        temp += class_scatter
+    return temp
+
+
+def computeBetweenClassScatterMatrices(labelled_images, mean_vectors):
+    overall_mean = np.mean(labelled_images[:, 1:], axis=0)
+    feature_count = labelled_images.shape[1] - 1
+    temp = np.zeros((feature_count, feature_count))
+
+    for label, mean in enumerate(mean_vectors):
+        n = labelled_images[labelled_images[:, 0] == label, 1:].shape[0]
+        mean = mean.reshape(feature_count, 1)
+        overall_mean = overall_mean.reshape(feature_count, 1)
+        temp += n * (mean - overall_mean).dot((mean - overall_mean).T)
+    return temp
+
+
+def computeTopEigenvectors(within_matrix, between_matrix, N=40):
+    eigenvalues, eigenvectors = np.linalg.eig(
+        np.linalg.pinv(within_matrix).dot(between_matrix)
+    )
+
+    eigenpairs = [
+        (np.abs(eigenvalues[i]), eigenvectors[:, i]) for i in range(len(eigenvalues))
+    ]
+    eigenpairs = sorted(eigenpairs, key=lambda k: k[0], reverse=True)
+
+    return np.hstack(
+        [eigenpairs[i][1].reshape(within_matrix.shape[0], 1) for i in range(N)]
+    )
+
+
 def image_to_reduced_feature(images, labels=None, split="train"):
     """
     Finds the most suitable features, reducing to N-features using PCA/LDA
     By default the images are 28x28 pixels
     """
 
-    v = calculatePrincipalComponents(images, 100)
-    pca_train_data = projectToPrincipalComponents(images, v)
-    # add labels to the first column
-    labelled_images = addLabels(pca_train_data, labels)
-    print(labelled_images.shape)
-    best_features = findBestFeatures(labelled_images)
-    print(best_features)
+    labelled_images = addLabels(images, labels)
+    mean_vectors = computeMeanVectors(labelled_images)
+    within = computeWithinClassScatterMatrices(labelled_images, mean_vectors)
+    between = computeBetweenClassScatterMatrices(labelled_images, mean_vectors)
 
+    print(within.shape)
+    print(between.shape)
+
+    W = computeTopEigenvectors(within, between, N=40)
+    data_lda = labelled_images[:, 1:].dot(W)
+    labelled_lda = addLabels(data_lda, labels, datatype=complex)
+
+    best_features = findBestFeatures(labelled_lda)
     print(images[:, best_features].shape)
 
     return images[:, best_features]
