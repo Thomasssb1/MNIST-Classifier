94e72ad4e90b52a9f0f165ac7ea0713e8281da73
Thomas Beer <thomas.beer04@outlook.com>
2024-12-23 20:04:32 +0000

    Noise and mask injection

--- system.py
+++ system.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 
-class CNNModel:
+class KNNModel:
     def __init__(self, train_vectors, train_labels):
         self.train_vectors = train_vectors
         self.train_labels = train_labels
@@ -23,38 +23,43 @@ class CNNModel:
         modtrain = np.sqrt(np.sum(self.train_vectors * self.train_vectors, axis=1))
 
         dist = x / np.outer(modtest, modtrain.transpose())
-        sorted_dists = np.argsort(dist, axis=1)[:, -k]
+        print(dist.shape)
+        sorted_dist = np.argsort(dist, axis=1)[:, -k:]
 
         final_labels = []
-        for i in range(len(sorted_dists)):
-            mode_label = CNNModel.mode_numpy(sorted_dists[i])
-            final_labels.append(self.train_labels[mode_label])
+        for i in range(len(sorted_dist)):
+            mode_label = KNNModel.mode_numpy(self.train_labels[sorted_dist[i]])
+            final_labels.append(mode_label)
 
         return np.array(final_labels)
 
 
 class LDA:
     def __init__(self, images, labels):
-        self.labelled_images = addLabels(images, labels)
+        self.images = images
+        self.images_mean = np.mean(images, axis=0)
+        self.images_std = np.sqrt(np.var(images, axis=0))
+        self.images_std[self.images_std == 0] = 1
         self.labels = labels
+        self.W = None
 
     def _computeMeanVectors(self):
-        temp = np.zeros((10, self.labelled_images.shape[1] - 1))
+        unique_labels = np.unique(self.labels)
 
-        for label in range(0, 10):
-            mean = np.mean(
-                self.labelled_images[self.labelled_images[:, 0] == label, 1:], axis=0
-            )
-            temp[label, :] = mean
+        temp = [
+            np.mean(self.images[self.labels == label], axis=0)
+            for label in unique_labels
+        ]
         return temp
 
     def _computeWithinClassScatterMatrices(self, mean_vectors):
-        feature_count = self.labelled_images.shape[1] - 1
+        feature_count = self.images.shape[1]
+        unique_labels = np.unique(self.labels)
         temp = np.zeros((feature_count, feature_count))
 
-        for label, mean in zip(range(0, 10), mean_vectors):
+        for label, mean in zip(unique_labels, mean_vectors):
             class_scatter = np.zeros(temp.shape)
-            for row in self.labelled_images[self.labelled_images[:, 0] == label, 1:]:
+            for row in self.images[self.labels == label]:
                 row, mean = row.reshape(feature_count, 1), mean.reshape(
                     feature_count, 1
                 )
@@ -63,18 +68,19 @@ class LDA:
         return temp
 
     def _computeBetweenClassScatterMatrices(self, mean_vectors):
-        overall_mean = np.mean(self.labelled_images[:, 1:], axis=0)
-        feature_count = self.labelled_images.shape[1] - 1
+        overall_mean = np.mean(self.images, axis=0)
+        feature_count = self.images.shape[1]
+        unique_labels = np.unique(self.labels)
         temp = np.zeros((feature_count, feature_count))
 
-        for label, mean in enumerate(mean_vectors):
-            n = self.labelled_images[self.labelled_images[:, 0] == label, 1:].shape[0]
+        for label, mean in zip(unique_labels, mean_vectors):
+            n = self.images[self.labels == label, :].shape[0]
             mean = mean.reshape(feature_count, 1)
             overall_mean = overall_mean.reshape(feature_count, 1)
             temp += n * (mean - overall_mean).dot((mean - overall_mean).T)
         return temp
 
-    def _computeTopEigenvectors(within_matrix, between_matrix, N=40):
+    def _computeTopEigenvectors(within_matrix, between_matrix, N=5):
         eigenvalues, eigenvectors = np.linalg.eig(
             np.linalg.pinv(within_matrix).dot(between_matrix)
         )
@@ -89,18 +95,35 @@ class LDA:
             [eigenpairs[i][1].reshape(within_matrix.shape[0], 1) for i in range(N)]
         )
 
+    def standardiseVector(self, vector):
+        return np.array(
+            (vector - self.images_mean) / self.images_std,
+            dtype=float,
+        )
+
+    def centreData(self, vector):
+        return vector - self.images_mean
+
+    def projectData(self, vector):
+        if self.W is None:
+            raise Exception(
+                "Top eigenvectors for LDA need to be computed during the training stage."
+            )
+        return np.dot(vector, self.W)
+
     def compute(self):
         mean_vectors = self._computeMeanVectors()
         within = self._computeWithinClassScatterMatrices(mean_vectors)
         between = self._computeBetweenClassScatterMatrices(mean_vectors)
-        W = LDA._computeTopEigenvectors(within, between)
-        lda_data = self.labelled_images[:, 1:].dot(W)
-        return addLabels(lda_data, self.labels, datatype=complex)
+        self.W = LDA._computeTopEigenvectors(within, between)
+        print(self.W.shape)
 
 
 class PCA:
     def __init__(self, images, features):
         self.images = images
+        self.images_mean = np.mean(images)
+        self.images_std = np.sqrt(np.var(images))
         self.features = features
         self.v = None
 
@@ -120,13 +143,15 @@ class PCA:
             raise Exception(
                 "Principal components need to have been computed during the training stage."
             )
-        pca_train_data = np.dot((images - np.mean(self.images)), self.v)
+        pca_train_data = np.dot((images - self.images_mean), self.v)
         return pca_train_data
 
     def standardiseVector(self, vector):
-        return np.array(
-            (vector - np.mean(self.images)) / np.sqrt(np.var(self.images)), dtype=float
+        # std[std == 0] = 1
+        normal = np.array(
+            (vector - self.images_mean) / self.images_std,
         )
+        return normal
 
 
 def addLabels(images, labels, datatype=None):
@@ -166,6 +191,55 @@ def findBestFeatures(labelled_pca_images, N=9):
     return features
 
 
+def evenlyChooseImages(images, labels, amount):
+    unique_labels = np.unique(labels)
+    per_letter = amount // len(unique_labels)
+
+    new_images = None
+    new_labels = None
+
+    for i in range(len(unique_labels)):
+        indices = np.where(labels == unique_labels[i])[0]
+        random_indices = indices[:per_letter]
+
+        if new_images is None:
+            new_images = images[random_indices, :]
+        else:
+            new_images = np.vstack([new_images, images[random_indices, :]], dtype=float)
+
+        if new_labels is None:
+            new_labels = labels[random_indices]
+        else:
+            new_labels = np.hstack([new_labels, labels[random_indices]])
+
+    return new_images, new_labels
+
+
+def generateNoiseData(images, labels, amount=300, max_noise=5):
+    new_images, new_labels = evenlyChooseImages(images, labels, amount)
+
+    for i in range(new_images.shape[0]):
+        noise = np.random.normal(0, max_noise, new_images.shape[1])
+        new_images[i] += noise
+
+    return np.vstack([images, new_images]), np.hstack([labels, new_labels])
+
+
+def generateMaskedData(images, labels, amount=300, masked_pixels=180):
+    if masked_pixels > images.shape[1]:
+        masked_pixels = images.shape[1]
+
+    new_images, new_labels = evenlyChooseImages(images, labels, amount)
+
+    for i in range(new_images.shape[0]):
+        random_masks = np.random.choice(
+            new_images.shape[1], size=masked_pixels, replace=False
+        )
+        new_images[i, random_masks] = 0
+
+    return np.vstack([images, new_images]), np.hstack([labels, new_labels])
+
+
 def image_to_reduced_feature(images, labels=None, split="test"):
     """
     Finds the most suitable features, reducing to N-features using PCA/LDA
@@ -176,20 +250,26 @@ def image_to_reduced_feature(images, labels=None, split="test"):
     if split != "train":
         pca_model: PCA = load_model("pca_model.pkl")
         standardised_images = pca_model.standardiseVector(images)
-        pca_data = pca_model.projectToPrincipalComponents(standardised_images)
+        pca_data = pca_model.projectToPrincipalComponents(images)
     else:
-        pca_model = PCA(images, 20)
+        images, labels = generateNoiseData(images, labels)
+        # images, labels = generateMaskedData(images, labels, masked_pixels=100)
+
+        pca_model = PCA(images, 460)
         standardised_images = pca_model.standardiseVector(images)
         pca_model.images = standardised_images
         pca_model.calculatePrincipalComponents()
-        pca_data = pca_model.projectToPrincipalComponents(standardised_images)
+        pca_data = pca_model.projectToPrincipalComponents(images)
         save_model(pca_model, "pca_model.pkl")
 
-    labelled_pca = addLabels(pca_data, labels)
+    labelled_pca = addLabels(pca_data, labels, datatype=complex)
     best_features = findBestFeatures(labelled_pca, 9)
 
     return pca_data[:, best_features]
 
 
 def training_model(train_features, train_labels):
-    return CNNModel(train_features, train_labels)
+    old_train_features = train_features[:-300]
+    _, labels = generateNoiseData(old_train_features, train_labels)
+    # _, labels = generateMaskedData(old_train_features, labels)
+    return KNNModel(train_features, labels)
